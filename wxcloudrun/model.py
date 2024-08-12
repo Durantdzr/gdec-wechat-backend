8e4152d862c9eeef6f008203ae309c6541964014
durant <duzr@shdata.com>
2024-08-12 15:39:16 +0800

    初次提交

--- wxcloudrun/model.py
+++ wxcloudrun/model.py
@@ -4,12 +4,15 @@ from wxcloudrun import db
 
 
 # 计数表
-class Counters(db.Model):
+class ConferenceInfo(db.Model):
     # 设置结构体表格名称
-    __tablename__ = 'Counters'
+    __tablename__ = 'conference_information'
 
     # 设定结构体对应表格的字段
     id = db.Column(db.Integer, primary_key=True)
-    count = db.Column(db.Integer, default=1)
-    created_at = db.Column('createdAt', db.TIMESTAMP, nullable=False, default=datetime.now())
-    updated_at = db.Column('updatedAt', db.TIMESTAMP, nullable=False, default=datetime.now())
+    title = db.Column('title', db.String(100), nullable=True)
+    org = db.Column('org', db.String(50), nullable=True)
+    create_time = db.Column('create_time', db.TIMESTAMP, nullable=False, default=datetime.now)
+
+    def get(self):
+        return {'id': self.id, 'title': self.title, 'org': self.org, 'create_time': self.create_time.strftime('%Y-%m-%d')}
