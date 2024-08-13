ee1fe70e4dadb3f174f1028b20fc46d2d60807b5
durant <duzr@shdata.com>
2024-08-13 15:00:03 +0800

    大会会场日程

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ DEBUG = True
 username = os.environ.get("MYSQL_USERNAME", 'root')
 password = os.environ.get("MYSQL_PASSWORD", 'root')
 db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+
+ENV = os.environ.get("ENV", 'prod-8g6293i8889df8e0')
+COS_BUCKET = os.environ.get("COS_BUCKET", '7072-prod-8g6293i8889df8e0-1259781903')
