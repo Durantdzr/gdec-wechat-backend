f192cc1116a67c19b337ab1121c0d7308a14b8c2
durant <duzr@shdata.com>
2024-08-16 18:55:09 +0800

    fix：搜索功能列表，用户未审核无法查询

--- wxcloudrun/dao.py
+++ wxcloudrun/dao.py
@@ -33,7 +33,7 @@ def search_friends_byopenid(openid, name):
     for friend in friends:
         friend_list.append(friend.operater_id)
         friend_list.append(friend.inviter_id)
-    socail_user = User.query.filter(User.name.like('%' + name + '%'), User.is_deleted == 0,
+    socail_user = User.query.filter(User.name.like('%' + name + '%'), User.status == 2, User.is_deleted == 0,
                                     User.socail == 1, ~User.id.in_(friend_list)).all()
     return socail_user
 
@@ -48,14 +48,14 @@ def get_friend_list(openid, name):
     for relation, user in operator_friends:
         data.append({"name": user.name, "id": user.id, "company": user.company, "title": user.title,
                      "img_url": 'https://{}.tcb.qcloud.la/{}'.format(config.COS_BUCKET, user.img_url),
-                     "status": status_ENUM.get(relation.status),"relation_id":relation.id})
+                     "status": status_ENUM.get(relation.status), "relation_id": relation.id})
     invited_friends = db.session.query(RelationFriend, User).join(User, User.id == RelationFriend.operater_id).filter(
         User.name.like('%' + name + '%'), RelationFriend.inviter_id == user_id, RelationFriend.is_deleted == 0).all()
     status_ENUM = {0: '接受邀请', 1: '已添加'}
     for relation, user in invited_friends:
         data.append({"name": user.name, "id": user.id, "company": user.company, "title": user.title,
                      "img_url": 'https://{}.tcb.qcloud.la/{}'.format(config.COS_BUCKET, user.img_url),
-                     "status": status_ENUM.get(relation.status),"relation_id":relation.id})
+                     "status": status_ENUM.get(relation.status), "relation_id": relation.id})
     return data
 
 
@@ -77,14 +77,16 @@ def save_realtion_friendbyid(id):
     :return: Counter实体
     """
     try:
-        record = RelationFriend.query.filter(RelationFriend.id==id).first()
+        record = RelationFriend.query.filter(RelationFriend.id == id).first()
         record.status = 1
         db.session.commit()
         return record.id
     except OperationalError as e:
         logger.info("query_counterbyid errorMsg= {} ".format(e))
         return None
-def is_invited_user(relation_id,wxopenid):
+
+
+def is_invited_user(relation_id, wxopenid):
     result = db.session.query(RelationFriend, User).join(User, User.id == RelationFriend.inviter_id).filter(
         User.openid == wxopenid, RelationFriend.id == relation_id).all()
     if len(result) > 0:
@@ -92,7 +94,8 @@ def is_invited_user(relation_id,wxopenid):
     else:
         return False
 
-def update_user_statusbyid(userlist,status):
+
+def update_user_statusbyid(userlist, status):
     """
     :param id: Counter的ID
     :return: Counter实体
@@ -107,7 +110,8 @@ def update_user_statusbyid(userlist,status):
         logger.info("query_counterbyid errorMsg= {} ".format(e))
         return None
 
+
 def get_guests_list():
-    guests = User.query.filter(User.type=='嘉宾',User.is_deleted==0).all()
+    guests = User.query.filter(User.type == '嘉宾', User.is_deleted == 0).all()
     data = [guest.get_guest() for guest in guests]
     return data
