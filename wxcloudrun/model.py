214db9e1148821ca5dace509e9f63eb986d0c4dc
durant <duzr@shdata.com>
2024-08-21 14:12:19 +0800

    feature：日程直播状态

--- wxcloudrun/model.py
+++ wxcloudrun/model.py
@@ -68,128 +68,125 @@ class ConferenceSchedule(db.Model):
     def get_schedule_view(self):
         status_ENUM = {0: '我要报名', 1: '正在直播' if self.live_status == 1 else '会议进行中',
                        2: '查看回放' if self.live_status == 2 else '会议结束'}
+        live_status_ENUM = {1: '未开始', 2: '直播中', 3: '回放中'}
         if self.guest is None:
             guest_id = []
         else:
             guest_id = list(map(int, self.guest.split(',')))
         if '开幕式' in self.title:
-            ext='开幕式'
+            ext = '开幕式'
         elif '主会场' in self.hall:
-            ext='主论坛'
+            ext = '主论坛'
         else:
-            ext=''
+            ext = ''
         return {'id': self.id, 'title': self.title, 'location': self.location, "hall": self.hall,
                 'conference_date': self.conference_date.strftime('%Y-%m-%d'), 'status': status_ENUM.get(self.status),
                 "begin_time": self.begin_time, "end_time": self.end_time, 'live_url': self.live_url,
-                "record_url": self.record_url, 'guest_id': guest_id,'ext':ext}
+                "record_url": self.record_url, 'guest_id': guest_id, 'ext': ext,
+                'live_status': live_status_ENUM.get(self.live_status, '')}
 
+    class ConferenceHall(db.Model):
+        # 设置结构体表格名称
+        __tablename__ = 'conference_hall'
 
-class ConferenceHall(db.Model):
-    # 设置结构体表格名称
-    __tablename__ = 'conference_hall'
+        # 设定结构体对应表格的字段
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column('name', db.String(50), nullable=True)
 
-    # 设定结构体对应表格的字段
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column('name', db.String(50), nullable=True)
+    class User(db.Model):
+        # 设置结构体表格名称
+        __tablename__ = 't_user'
+        # 设定结构体对应表格的字段
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column('name', db.String(100), nullable=True)
+        openid = db.Column('openid', db.String(50), nullable=True)
+        phone = db.Column('phone', db.String(20), nullable=True)
+        code = db.Column('code', db.String(50), nullable=True)
+        company = db.Column('company', db.String(100), nullable=True)
+        title = db.Column('title', db.String(255), nullable=True)
+        type = db.Column('type', db.String(10), default='普通观众')
+        socail = db.Column('socail', db.INT, default=0)
+        is_deleted = db.Column('is_deleted', db.INT, default=0)
+        img_url = db.Column('img_url', db.TEXT)
+        status = db.Column('status', db.INT, default=0)
+        password = db.Column('pwd', db.String(50), nullable=True)
+        guest_info = db.Column('guest_info', db.TEXT)
+        order = db.Column('order', db.Integer, default=0)
+        forum = db.Column('forum', db.String)
 
+        def get_status(self):
+            status_ENUM = {1: '审核未通过', 2: '审核已通过', 0: '未审核'}
+            return status_ENUM.get(self.status, '审核未通过')
 
-class User(db.Model):
-    # 设置结构体表格名称
-    __tablename__ = 't_user'
-    # 设定结构体对应表格的字段
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column('name', db.String(100), nullable=True)
-    openid = db.Column('openid', db.String(50), nullable=True)
-    phone = db.Column('phone', db.String(20), nullable=True)
-    code = db.Column('code', db.String(50), nullable=True)
-    company = db.Column('company', db.String(100), nullable=True)
-    title = db.Column('title', db.String(255), nullable=True)
-    type = db.Column('type', db.String(10), default='普通观众')
-    socail = db.Column('socail', db.INT, default=0)
-    is_deleted = db.Column('is_deleted', db.INT, default=0)
-    img_url = db.Column('img_url', db.TEXT)
-    status = db.Column('status', db.INT, default=0)
-    password = db.Column('pwd', db.String(50), nullable=True)
-    guest_info = db.Column('guest_info', db.TEXT)
-    order = db.Column('order', db.Integer, default=0)
-    forum = db.Column('forum', db.String)
+        def get(self):
+            return {"id": self.id, "name": self.name, "company": self.company, "title": self.title,
+                    "img_url": 'https://{}.tcb.qcloud.la/{}'.format(config.COS_BUCKET, self.img_url)}
 
-    def get_status(self):
-        status_ENUM = {1: '审核未通过', 2: '审核已通过', 0: '未审核'}
-        return status_ENUM.get(self.status, '审核未通过')
+        def get_full(self):
+            return {"id": self.id, "name": self.name, "company": self.company, "title": self.title, "phone": self.phone,
+                    "code": self.code, "type": self.type,
+                    "img_url": 'https://{}.tcb.qcloud.la/{}'.format(config.COS_BUCKET, self.img_url),
+                    'cdn_param': self.img_url}
 
-    def get(self):
-        return {"id": self.id, "name": self.name, "company": self.company, "title": self.title,
-                "img_url": 'https://{}.tcb.qcloud.la/{}'.format(config.COS_BUCKET, self.img_url)}
+        def get_guest(self):
+            return {"id": self.id, "name": self.name, "company": self.company, "title": self.title,
+                    "info": self.guest_info,
+                    "img_url": 'https://{}.tcb.qcloud.la/{}'.format(config.COS_BUCKET, self.img_url),
+                    'cdn_param': self.img_url, 'forum': self.forum, 'order': self.order}
 
-    def get_full(self):
-        return {"id": self.id, "name": self.name, "company": self.company, "title": self.title, "phone": self.phone,
-                "code": self.code, "type": self.type,
-                "img_url": 'https://{}.tcb.qcloud.la/{}'.format(config.COS_BUCKET, self.img_url),
-                'cdn_param': self.img_url}
+    class RelationFriend(db.Model):
+        # 设置结构体表格名称
+        __tablename__ = 'r_user_friend'
+        # 设定结构体对应表格的字段
+        id = db.Column(db.Integer, primary_key=True)
+        operater_id = db.Column('operater_id', db.Integer)
+        inviter_id = db.Column('inviter_id', db.Integer)
+        meeting_date = db.Column('meeting_date', db.TIMESTAMP, nullable=True)
+        create_time = db.Column('create_time', db.TIMESTAMP, nullable=True, default=datetime.now)
+        visit_info = db.Column('visit_info', db.String(100), nullable=True)
+        is_deleted = db.Column('is_deleted', db.INT, default=0)
+        status = db.Column('status', db.INT, default=0)
 
-    def get_guest(self):
-        return {"id": self.id, "name": self.name, "company": self.company, "title": self.title, "info": self.guest_info,
-                "img_url": 'https://{}.tcb.qcloud.la/{}'.format(config.COS_BUCKET, self.img_url),
-                'cdn_param': self.img_url, 'forum': self.forum, 'order': self.order}
+    class ConferenCoopearter(db.Model):
+        # 设置结构体表格名称
+        __tablename__ = 'conference_coopearter'
+        # 设定结构体对应表格的字段
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column('name', db.String(30), nullable=True)
+        img_url = db.Column('img_url', db.String(100), nullable=True)
+        url = db.Column('url', db.String(50), nullable=True)
+        type = db.Column('type', db.String(10), nullable=True)
+        is_deleted = db.Column('is_deleted', db.INT, default=0)
 
+        def get(self):
+            return {"id": self.id, "name": self.name, "cdn_param": self.img_url, "type": self.type,
+                    "img_url": 'https://{}.tcb.qcloud.la/{}'.format(config.COS_BUCKET, self.img_url), "url": self.url}
 
-class RelationFriend(db.Model):
-    # 设置结构体表格名称
-    __tablename__ = 'r_user_friend'
-    # 设定结构体对应表格的字段
-    id = db.Column(db.Integer, primary_key=True)
-    operater_id = db.Column('operater_id', db.Integer)
-    inviter_id = db.Column('inviter_id', db.Integer)
-    meeting_date = db.Column('meeting_date', db.TIMESTAMP, nullable=True)
-    create_time = db.Column('create_time', db.TIMESTAMP, nullable=True, default=datetime.now)
-    visit_info = db.Column('visit_info', db.String(100), nullable=True)
-    is_deleted = db.Column('is_deleted', db.INT, default=0)
-    status = db.Column('status', db.INT, default=0)
+    class ConferenceSignUp(db.Model):
+        # 设置结构体表格名称
+        __tablename__ = 'conference_sign_up'
+        # 设定结构体对应表格的字段
+        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column('user_id', db.Integer)
+        schedule_id = db.Column('schedule_id', db.Integer)
+        status = db.Column('status', db.Integer, default=0)
 
+    class Media(db.Model):
+        # 设置结构体表格名称
+        __tablename__ = 'manage_media'
+        # 设定结构体对应表格的字段
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column('name', db.String(50), nullable=True)
+        info = db.Column('info', db.String(100), nullable=True)
+        type = db.Column('type', db.String(10), nullable=True)
+        media_param = db.Column('media_param', db.TEXT, nullable=True)
+        is_deleted = db.Column('is_deleted', db.INT, default=0)
 
-class ConferenCoopearter(db.Model):
-    # 设置结构体表格名称
-    __tablename__ = 'conference_coopearter'
-    # 设定结构体对应表格的字段
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column('name', db.String(30), nullable=True)
-    img_url = db.Column('img_url', db.String(100), nullable=True)
-    url = db.Column('url', db.String(50), nullable=True)
-    type = db.Column('type', db.String(10), nullable=True)
-    is_deleted = db.Column('is_deleted', db.INT, default=0)
-
-    def get(self):
-        return {"id": self.id, "name": self.name, "cdn_param": self.img_url, "type": self.type,
-                "img_url": 'https://{}.tcb.qcloud.la/{}'.format(config.COS_BUCKET, self.img_url), "url": self.url}
-
-
-class ConferenceSignUp(db.Model):
-    # 设置结构体表格名称
-    __tablename__ = 'conference_sign_up'
-    # 设定结构体对应表格的字段
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column('user_id', db.Integer)
-    schedule_id = db.Column('schedule_id', db.Integer)
-    status = db.Column('status', db.Integer, default=0)
-
-
-class Media(db.Model):
-    # 设置结构体表格名称
-    __tablename__ = 'manage_media'
-    # 设定结构体对应表格的字段
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column('name', db.String(50), nullable=True)
-    info = db.Column('info', db.String(100), nullable=True)
-    type = db.Column('type', db.String(10), nullable=True)
-    media_param = db.Column('media_param', db.TEXT, nullable=True)
-    is_deleted = db.Column('is_deleted', db.INT, default=0)
-
-    def get(self):
-        if self.type == '图片':
-            return {"id": self.id, "name": self.name, "info": self.info, "type": self.type,
-                    "cdn_param": self.media_param,
-                    "web_url": 'https://{}.tcb.qcloud.la/web/{}'.format(config.COS_BUCKET, self.id)}
-        else:
-            return {"id": self.id, "name": self.name, "info": self.info, "type": self.type, "doc": self.media_param,
-                    "web_url": 'https://{}.tcb.qcloud.la/web/{}'.format(config.COS_BUCKET, self.id)}
+        def get(self):
+            if self.type == '图片':
+                return {"id": self.id, "name": self.name, "info": self.info, "type": self.type,
+                        "cdn_param": self.media_param,
+                        "web_url": 'https://{}.tcb.qcloud.la/web/{}'.format(config.COS_BUCKET, self.id)}
+            else:
+                return {"id": self.id, "name": self.name, "info": self.info, "type": self.type, "doc": self.media_param,
+                        "web_url": 'https://{}.tcb.qcloud.la/web/{}'.format(config.COS_BUCKET, self.id)}
