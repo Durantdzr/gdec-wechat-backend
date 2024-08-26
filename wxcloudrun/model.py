2828db2679fa05162510ea0ca5d436e1bcdcbe85
durant <duzr@shdata.com>
2024-08-26 16:31:17 +0800

    会议直击新增图片地址

--- wxcloudrun/model.py
+++ wxcloudrun/model.py
@@ -55,7 +55,8 @@ class ConferenceSchedule(db.Model):
     def get_live(self):
         status_ENUM = {1: '即将直播', 2: '正在直播', 3: '查看回放'}
         return {'id': self.id, 'title': self.title, 'location': self.location, 'live_time': self.get_live_time(),
-                'status': status_ENUM.get(self.live_status), 'live_url': self.live_url, 'record_url': self.record_url}
+                'status': status_ENUM.get(self.live_status), 'live_url': self.live_url, 'record_url': self.record_url,
+                "img_url": 'https://{}.tcb.qcloud.la/{}'.format(config.COS_BUCKET, self.img_url)}
 
     def get_live_time(self):
         return self.conference_date.strftime('%Y年%m月%d日') + '(' + WEEKDAY.get(
