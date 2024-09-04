a2311ec34354b56c781ea69a534be756f38d96cb
durant <duzr@shdata.com>
2024-09-04 10:01:41 +0800

    fix:配置文件为空

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 DEBUG = False
 
 # 读取数据库环境变量
-username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", 'root')
-db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+username = os.environ.get("MYSQL_USERNAME", '')
+password = os.environ.get("MYSQL_PASSWORD", '')
+db_address = os.environ.get("MYSQL_ADDRESS", '')
 
 ENV = os.environ.get("ENV", '')
 COS_BUCKET = os.environ.get("COS_BUCKET", '')
