94f8bde2a07e8e78d9d40d4a6d52b006664fe224
durant <duzr@shdata.com>
2024-10-14 17:04:47 +0800

    启动项run文件修改

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import sys
 from wxcloudrun import app
 # 启动Flask Web服务
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='80')
-    # app.run(host=sys.argv[1], port=sys.argv[2], use_reloader=False)
+    # app.run(host='0.0.0.0', port='80')
+    app.run(host=sys.argv[1], port=sys.argv[2], use_reloader=False)
     
