1b76a6c8bf6751c4119e32e0c7dba18a7f10ce81
durant <duzr@shdata.com>
2024-10-14 15:04:29 +0800

    日志打印

--- wxcloudrun/cronjob.py
+++ wxcloudrun/cronjob.py
@@ -9,16 +9,20 @@
 
 from wxcloudrun.dao import find_user_schedule_tobegin,get_user_picture
 from wxcloudrun.utils import send_to_begin_msg,download_cdn_file
+import logging
+logger = logging.getLogger('log')
 def send_begin_msg():
     result = find_user_schedule_tobegin()
     for item in result:
         send_to_begin_msg(item.get("openid",""), item.get("title",""), item.get("location",""), item.get("begin_time",""))
 
 def reload_image():
+    logger.info("获取用户照片")
     img_url=get_user_picture()
     img_url=list(set(img_url))
     for user in img_url:
         if user is not None:
             download_cdn_file(user, user)
+    logger.info("完成获取用户照片")
 
     
