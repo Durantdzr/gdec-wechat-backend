ab132239640f66cddc07b087b60321b343ff76f2
durant <duzr@shdata.com>
2024-10-15 16:55:28 +0800

    fix:配置文件修改

--- config.py
+++ config.py
@@ -4,15 +4,19 @@ import os
 DEBUG = True
 
 # 读取数据库环境变量
-username = os.environ.get("MYSQL_USERNAME", 'shdata')
-password = os.environ.get("MYSQL_PASSWORD", 'Qwer123$')
-db_address = os.environ.get("MYSQL_ADDRESS", 'sh-cynosdbmysql-grp-m9l8ybb0.sql.tencentcdb.com:27614')
-database = os.environ.get("MYSQL_database", 'GDEC-test')
+username = os.environ.get("MYSQL_USERNAME", '')
+password = os.environ.get("MYSQL_PASSWORD", '')
+db_address = os.environ.get("MYSQL_ADDRESS", '')
+database = os.environ.get("MYSQL_database", '')
 ENV = os.environ.get("ENV", '')
-COS_BUCKET = os.environ.get("COS_BUCKET", '7072-prod-5g9xtpt32ae0a1e3-1328915519')
+COS_BUCKET = os.environ.get("COS_BUCKET", '')
 JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", 'your-secret-key')
-FERNET_KEY = os.environ.get("FERNET_KEY", 'bCYvy-zUKWPlf0_KC4yiJYDcIXU1IsM1cvjf5hpo7tQ=')
+FERNET_KEY = os.environ.get("FERNET_KEY", '')
 
 CORS_ORIGINS = os.environ.get("CORS_ORIGINS", '*')
 
 VERSION = os.environ.get("VERSION", '')
+
+SecretId = os.environ.get("SecretId", '')
+SecretKey = os.environ.get("SecretKey", '')
+SdkAppId= os.environ.get("SdkAppId", '')
