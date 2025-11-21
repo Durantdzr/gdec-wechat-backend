67cdf87ba2f03d1d48071a044fb4183ca52caa83
durant <duzr@shdata.com>
2025-11-21 16:46:12 +0800

    refactor(mangerviews): 注释掉用户图片处理逻辑
    - 注释了用户头像复制和下载的代码块
    - 保留了用户信息导出到DataFrame的功能
    - 未改变数据导出的字段和结构

--- wxcloudrun/mangerviews.py
+++ wxcloudrun/mangerviews.py
@@ -1093,11 +1093,11 @@ def download_user_list():
     os.mkdir(now)
     os.mkdir('{}/guest'.format(now))
     for user in users:
-        if user.img_url is not None:
-            if os.path.exists(user.img_url):
-                shutil.copy2(user.img_url, '{}/{}'.format(now, user.img_url))
-            else:
-                download_cdn_file(user.img_url, '{}/{}'.format(now, user.img_url))
+        # if user.img_url is not None:
+        #     if os.path.exists(user.img_url):
+        #         shutil.copy2(user.img_url, '{}/{}'.format(now, user.img_url))
+        #     else:
+        #         download_cdn_file(user.img_url, '{}/{}'.format(now, user.img_url))
         df = df.append({"序号": user.id, "员工编号": user.id, "姓名": user.name, "性别": "男", "电话号码": user.phone,
                         "证件类型": "身份证" if user.code is None or len(user.code) == 18 else '普通护照',
                         "证件号码": user.code, "用户类型": user.type, "单位": user.company, "职务": user.title,
